63d268a8bf080809de621f4ee2d81950543cddf9
Mehdi Bukhari <mehdi@maservices-ltd.com>
2020-03-14 20:28:38 +0500

    changes

--- src/Notes/DL/INOTES.ts
+++ src/Notes/DL/INOTES.ts
@@ -1,15 +1,15 @@
-import { Document } from "mongoose";
-import {IUserModel} from "../../User/DL/user"
-import { IJOB } from "../../Job/DL/IJOB";
-import { IADMIN } from "../../admin/DL/IAdmin";
-import { Iconsaltants } from "src/consaltant/DL/consaltants";
-export interface INOTES extends Document{
-    _id:string,
-    CommentText:string,
-    displayingId:IUserModel["_id"]
-    CommetingPersonId:IUserModel["_id"]
-    createdAt:string
-    updatedAt:string
-    updateby:string
-    createdby:string
+import { Document } from 'mongoose';
+import { IUserModel } from '../../User/DL/user';
+import { IJOB } from '../../Job/DL/IJOB';
+import { IADMIN } from '../../admin/DL/IAdmin';
+import { Iconsaltants } from 'src/consaltant/DL/consaltants';
+export interface INOTES extends Document {
+    _id: string;
+    NoteText: string;
+    displayingId: IUserModel['_id'];
+    NotesPersonId: IUserModel['_id'];
+    createdAt: string;
+    updatedAt: string;
+    updateby: string;
+    createdby: string;
 }
